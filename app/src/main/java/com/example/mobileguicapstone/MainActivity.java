eec49ca8c534ec98b60c1f237ff77f512815bdb6
jolenic <jennifer.olenic@gmail.com>
2020-11-07 20:29:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mobileguicapstone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileguicapstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
