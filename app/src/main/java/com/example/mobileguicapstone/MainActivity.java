3b8484b573c39259aa0fc4b2d6afaf3283035269
toomanycorgis <aria.burton95@gmail.com>
2020-11-13 10:20:56 -0500

    added some initial skeleton objects and created a button to the class where our initial code can go.

--- app/src/main/java/com/example/mobileguicapstone/MainActivity.java
+++ app/src/main/java/com/example/mobileguicapstone/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mobileguicapstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.enterAppButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NasaDailyImage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mobileguicapstone/NasaDailyImage.java
@@ -0,0 +1,15 @@
+package com.example.mobileguicapstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NasaDailyImage extends AppCompatActivity {
+
+    // placeholder class where the initial stuff for the image of the day loading class will go
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daily_image_main);
+    }
+}
