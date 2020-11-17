1e6a3612432dd98b5fe8f468dab2a82d1f61125b
jolenic <jennifer.olenic@gmail.com>
2020-11-17 14:27:47 -0500

    Copied in placeholder for image display and button
    Signed-off-by: jolenic <jennifer.olenic@gmail.com>

---
+++ app/src/main/java/com/example/mobileguicapstone/NasaDailyImage.java
@@ -0,0 +1,14 @@
+package com.example.mobileguicapstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NasaDailyImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daily_image_main);
+    }
+}
